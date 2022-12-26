5599ef67d2ae2c8a46de1a9988586bb541328a64
Alessandro Lima Nobre <alessandronobre.ti@gmail.com>
2022-12-26 20:20:14 -0300

    primeiro commit

---
+++ src/main/java/br/com/macrosxtreme/MacrosXtremeApplication.java
@@ -0,0 +1,13 @@
+package br.com.macrosxtreme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacrosXtremeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacrosXtremeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/macrosxtreme/MacrosXtremeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.macrosxtreme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacrosXtremeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
