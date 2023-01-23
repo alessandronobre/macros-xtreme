2057651b3db90992395396969aa764fe38a8b780
Alessandro Lima Nobre <alessandronobre.ti@gmail.com>
2023-01-23 16:02:50 -0300

    teste

--- src/main/java/br/com/macrosxtreme/controller/LoginController.java
+++ src/main/java/br/com/macrosxtreme/controller/LoginController.java
@@ -18,7 +18,7 @@ public class LoginController {
 	
 
 //	Access Aplication
-	@GetMapping("/login")
+	@GetMapping("/logins")
 	public ModelAndView login() {
 		ModelAndView modelAndView = new ModelAndView("login/login");
 		return modelAndView;
